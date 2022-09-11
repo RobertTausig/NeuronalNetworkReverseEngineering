e3bb642e6b5d7cca22d4cbda8d7b4e735495de3c
Robert Tausig <sicherklar-microsoft@yahoo.de>
2022-09-11 14:24:28 +0200

    Add project files.

---
+++ Matrix.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuronalNetworkReverseEngineering
+{
+    public class Matrix
+    {
+        public Matrix(int x, int y)
+        {
+            content = new double[x, y];
+        }
+
+        private double[,] content;
+
+        public void SetValue(int x, int y, double value)
+        {
+            content[x, y] = value;
+        }
+
+
+    }
+}
