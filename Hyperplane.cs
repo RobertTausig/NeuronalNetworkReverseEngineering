1bda13bf3fa300c2d4b57f38a9c95b08f5ac9a71
Robert Tausig <sicherklar-microsoft@yahoo.de>
2022-09-20 19:56:15 +0200

    Cleanup.

--- Hyperplane.cs
+++ Hyperplane.cs
@@ -18,19 +18,27 @@ namespace NeuronalNetworkReverseEngineering
         private Model model { get; }
         private SamplingLine sampler { get; }
 
+        private const int saltIncreasePerRecursion = 1_000;
+        private const int maxSalt = 4*saltIncreasePerRecursion;
+
         public List<Matrix> SupportPointsOnBoundary(Matrix boundaryPoint, int salt = 0)
         {
             if (!(boundaryPoint.numRow == 1 || boundaryPoint.numRow == 1))
             {
                 return null;
             }
+            if (salt > maxSalt)
+            {
+                throw new Exception("PM06");
+            }
 
             var retVal = new List<Matrix>();
             var bag = new ConcurrentBag<List<Matrix>>();
-            var iterations = boundaryPoint.numRow + boundaryPoint.numCol + 6;
+            var iterations = boundaryPoint.numRow + boundaryPoint.numCol + 10;
             var result = Parallel.For(0, iterations, index =>
             {
-                var tempModel = model.Copy(index+salt);
+                var tempModel = model.Copy(index + salt);
+                var tempSampler = new SamplingLine(tempModel);
 
                 var displacementVector = new Matrix(boundaryPoint.numRow, boundaryPoint.numCol);
                 displacementVector.PopulateAllRandomlyFarFromZero(tempModel.RandomGenerator);
@@ -41,11 +49,10 @@ namespace NeuronalNetworkReverseEngineering
                 directionVector = Matrix.NormalizeVector(directionVector, 0.02);
 
                 var startPoint = Matrix.Addition(boundaryPoint, displacementVector);
-                var tempSampler = new SamplingLine(tempModel);
-                var dd = tempSampler.BidirectionalLinearRegionChanges(startPoint, directionVector, 8);
-                bag.Add(dd);
+                var supportPoints = tempSampler.BidirectionalLinearRegionChanges(startPoint, directionVector, 8);
+                bag.Add(supportPoints);
             });
-            //Debug:
+
             if (result.IsCompleted)
             {
                 foreach (var item in bag)
@@ -55,10 +62,10 @@ namespace NeuronalNetworkReverseEngineering
                         retVal.AddRange(item);
                     }
                 }
-
-                if (retVal.Count < (boundaryPoint.numRow + boundaryPoint.numCol - 1))
+                //Mathematical minimum: retVal.Count < (boundaryPoint.numRow + boundaryPoint.numCol - 1)
+                if (retVal.Count < (boundaryPoint.numRow + boundaryPoint.numCol + 2))
                 {
-                    salt += 1_000;
+                    salt += saltIncreasePerRecursion;
                     return SupportPointsOnBoundary(boundaryPoint, salt);
                 }
                 Console.WriteLine(retVal.Count);
