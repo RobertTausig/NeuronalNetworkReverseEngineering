dc095df569fa701c808e32f7e7e1f66a5522b607
Robert Tausig <sicherklar-microsoft@yahoo.de>
2022-09-30 16:32:29 +0200

    Keep found support points over iterations in "SupportPointsOnBoundary".

--- Hyperplane.cs
+++ Hyperplane.cs
@@ -21,12 +21,13 @@ namespace NeuronalNetworkReverseEngineering
 
         private Model model { get; }
         public List<Matrix> pointsOnPlane { get; } = new List<Matrix>();
+        private List<Matrix> temporaryPointsOnPlane = new List<Matrix>();
         public (Matrix parameters, double? intercept) planeIdentity { get; }
         public int spaceDim { get; }
         public Matrix originalBoundaryPoint { get; }
 
         private const int saltIncreasePerRecursion = 1_000;
-        private const int maxSalt = 6*saltIncreasePerRecursion;
+        private const int maxSalt = 4*saltIncreasePerRecursion;
 
         private List<Matrix> SupportPointsOnBoundary(Matrix boundaryPoint, int salt, double displacementNorm, double directionNorm, int maxMagnitude)
         {
@@ -70,16 +71,23 @@ namespace NeuronalNetworkReverseEngineering
                 {
                     return new List<Matrix>();
                 }
-                else if (fineSample.Count() > iterations * 0.5)
+                else if (fineSample.Count() + temporaryPointsOnPlane.Count > iterations * 0.5)
                 {
                     foreach (var item in fineSample)
                     {
                         retVal.AddRange(item);
                     }
+                    retVal.AddRange(temporaryPointsOnPlane);
+                    temporaryPointsOnPlane.Clear();
                     return retVal;
                 }
                 else
                 {
+                    foreach (var item in fineSample)
+                    {
+                        temporaryPointsOnPlane.AddRange(item);
+                    }
+
                     if (overshootSample.Count() > undershootSample.Count())
                     {
                         salt += saltIncreasePerRecursion;
