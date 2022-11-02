4a777754426b76fc9a4c8d59ae6aae1c81e013b7
Robert Tausig <sicherklar-microsoft@yahoo.de>
2022-11-02 21:38:51 +0100

    Improved "SamplingSphere".

--- SamplingSphere.cs
+++ SamplingSphere.cs
@@ -15,23 +15,28 @@ namespace NeuronalNetworkReverseEngineering
         }
 
         private Model model { get; }
-        int stdIterations = 100;
-        double stdIterationGrowth = 1.08;
-        int stdMaxMagnitude = 6;
+        private int stdMaxMagnitude = 6;
+        private int salt = 0;
+        private int saltIncreasePerUsage = 1_000;
 
         public double MinimumDistanceToDifferentBoundary(Matrix boundaryPoint, double startingDistance)
         {
             var retVal = new List<Matrix>();
+
             double directionNorm = startingDistance / Math.Pow(2, stdMaxMagnitude);
+            var spaceDim = boundaryPoint.numRow + boundaryPoint.numCol - 1;
+            var iterations = (spaceDim + 1) * 15;
+            var iterationGrowth = Math.Pow(2_500, 1.0 / iterations);
+
             var conc = new ConcurrentDictionary<int, List<Matrix>>();
-            var result = Parallel.For(0, stdIterations, index =>
+            var result = Parallel.For(0, iterations, index =>
             {
-                var tempModel = model.Copy(index);
+                var tempModel = model.Copy(index + salt);
                 var tempSampler = new SamplingLine(tempModel);
 
                 var directionVector = new Matrix(boundaryPoint.numRow, boundaryPoint.numCol);
                 directionVector.PopulateAllRandomlyFarFromZero(tempModel.RandomGenerator);
-                directionVector = Matrix.NormalizeVector(directionVector, directionNorm * Math.Pow(stdIterationGrowth, index));
+                directionVector = Matrix.NormalizeVector(directionVector, directionNorm * Math.Pow(iterationGrowth, index));
 
                 var samplePoints = tempSampler.BidirectionalLinearRegionChanges(boundaryPoint, directionVector, stdMaxMagnitude);
                 conc.TryAdd(index, samplePoints);
@@ -39,8 +44,9 @@ namespace NeuronalNetworkReverseEngineering
 
             if (result.IsCompleted)
             {
+                salt += saltIncreasePerUsage;
                 var temp = conc.First(x => x.Value.Count > 1).Key;
-                return startingDistance * Math.Pow(stdIterationGrowth, temp);
+                return startingDistance * Math.Pow(iterationGrowth, temp);
             }
             else
             {
