fa3f5f3a5f40218bb3c5c714a4fc0c233bea8ff4
Robert Tausig <sicherklar-microsoft@yahoo.de>
2023-05-16 14:33:35 +0200

    Add class "MathConvert" for converting from own classes to MathNet classes.

---
+++ MathConvert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinAlg = MathNet.Numerics.LinearAlgebra;
+
+namespace NeuronalNetworkReverseEngineering
+{
+    public class MathConvert
+    {
+        public static LinAlg.Matrix<double> MatrixToMatrix(Matrix input)
+        {
+            if (input.numRow == 1 || input.numCol == 1)
+            {
+                return null;
+            }
+            var retVal = LinAlg.Matrix<double>.Build.Dense(input.numRow, input.numCol);
+
+            for (int i = 0; i < input.numRow; i++)
+            {
+                for (int j = 0; j < input.numCol; j++)
+                {
+                    retVal[i, j] = input.content[i, j];
+                }
+            }
+            return retVal;
+        }
+    }
+}
