5236792ea2f39a2af3ebbcfc5fab2748ab5bdf2d
Robert Tausig <sicherklar-microsoft@yahoo.de>
2024-02-14 19:47:18 +0100

    Add logger for writing results to file. This will be very helpful when calculating in batch different topologies.

--- Program.cs
+++ Program.cs
@@ -8,20 +8,28 @@ namespace NeuronalNetworkReverseEngineering
 {
     class Program
     {
-        const int inputDim = 7;
-        const int firstLayerDim = 5;
-        const int secondLayerDim = 6;
-        const int outputDim = 4;
-
         static void Main(string[] args)
         {
+            int inputDim = 7;
+            int firstLayerDim = 5;
+            int secondLayerDim = 6;
+            int outputDim = 4;
+
+#if !DEBUG
+            if (args.Length != 4)
+            {
+                Console.WriteLine("Usage: Program <inputDim> <firstLayerDim> <secondLayerDim> <outputDim>");
+                return;
+            }
+            inputDim = int.Parse(args[0]);
+            firstLayerDim = int.Parse(args[1]);
+            secondLayerDim = int.Parse(args[2]);
+            outputDim = int.Parse(args[3]);
+#endif
             var clock = new Stopwatch();
             clock.Start();
 
-
-            var model = new Model(new int[4] { inputDim, firstLayerDim, secondLayerDim, outputDim }, hasBias: false);
-            //var ransac = new RansacAlgorithm(model);
-            //var isTestSuccess = ransac.Test();
+            var model = new Model([inputDim, firstLayerDim, secondLayerDim, outputDim], hasBias: false);
             var sampler = new SamplingLine(model);
             var sphere = new SamplingSphere(model);
             var layer = new LayerCalculation(model, sphere);
@@ -29,8 +37,6 @@ namespace NeuronalNetworkReverseEngineering
             int geometricArithmeticMean = (int)((Math.Sqrt(firstLayerDim * secondLayerDim) + (firstLayerDim + secondLayerDim) / 2) / 2);
             int numLines = geometricArithmeticMean + 5;
             var initialBundle = layer.DriveLinesThroughSpace(numLines: numLines);
-            // For debugging:
-            //var averageLinePoints = initialBundle.SpaceLines.Average(x => x.SpaceLinePoints.Count);
             var initialHyperplanesColl = layer.SpaceLinesToHyperplanes(initialBundle);
             var initialDistinctHyperplanes = layer.DistinctHyperplanes(initialHyperplanesColl);
             var firstLayerPlanes = layer.GetFirstLayer(initialDistinctHyperplanes);
@@ -42,10 +48,27 @@ namespace NeuronalNetworkReverseEngineering
             //var bb = bender.MoveAlongBend(outermostSecondLayerPlanes[0], firstLayerPlanes);
 
             clock.Stop();
-            Console.WriteLine("Time passed: " + clock.Elapsed.TotalMilliseconds);
-            Console.ReadLine();
-        }
+            Console.WriteLine("Time passed: " + clock.Elapsed.TotalSeconds);
 
+            var nowTime = System.DateTime.Now;
+            string fileName = $"{nowTime.ToString("yyMMdd")}_{nowTime.ToString("HHmmss")}_NNRE-Result_{inputDim}-{firstLayerDim}-{secondLayerDim}-{outputDim}.txt";
+            var logger = new VariablesLogger(fileName);
+            Dictionary<string, string> output = new Dictionary<string, string>
+            {
+                { "Time [seconds]", $"{clock.Elapsed.TotalSeconds}" },
+                { nameof(inputDim), $"{inputDim}" },
+                { nameof(firstLayerDim), $"{firstLayerDim}" },
+                { nameof(secondLayerDim), $"{secondLayerDim}" },
+                { nameof(outputDim), $"{outputDim}" },
+                { nameof(firstLayerPlanes) + "." + nameof(firstLayerPlanes.Count), $"{firstLayerPlanes.Count}" },
+                { nameof(analyzeResult.allFound), $"{analyzeResult.allFound}" },
+                { nameof(analyzeResult.recoveryRatio), $"{analyzeResult.recoveryRatio}" },
+                { nameof(analyzeResult.medianAccuracy), $"{analyzeResult.medianAccuracy}" },
+                { nameof(analyzeResult.meanAccuracy), $"{analyzeResult.meanAccuracy}" },
+                { nameof(analyzeResult.stdDeviationAccuracy), $"{analyzeResult.stdDeviationAccuracy}" },
+            };
+            logger.LogVariables(output);
+        }
 
 
         
---
+++ VariablesLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuronalNetworkReverseEngineering
+{
+    public class VariablesLogger
+    {
+        private readonly string filePath;
+
+        public VariablesLogger(string fileName)
+        {
+            this.filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+        }
+
+        public void LogVariables(Dictionary<string, string> variables)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var (Name, Value) in variables)
+            {
+                sb.Append(Name).Append("\t").Append(Value).AppendLine();
+            }
+
+            File.AppendAllText(filePath, sb.ToString());
+        }
+    }
+}
+
+
